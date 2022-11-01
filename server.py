9036bb9b5db0bc13e020ee3c916a5edd817199c3
rkinkk <r3kinek0@gmail.com>
2022-11-01 21:57:11 +0100

    a

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import os
 import time
 import socket
+import json
+
+datafile = open("data.json", "r+", encoding='utf-8')
+data = json.load(datafile)
 
 localIP=input("local ip: ")
 print(localIP)
@@ -16,4 +20,12 @@ while True:
     bap = ServerSock.recvfrom(messagesize)
     message=bap[0]
     addr=bap[1]
+
+    y=data[addr]
+
     print("message: ", message, " from: ", addr)
+
+datafile.seek(0)
+datafile.truncate()
+json.dump(data, datafile)
+datafile.close()
