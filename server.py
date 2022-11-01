72aaf30288a75abc2d19f4a091d0d09111dd4982
rkinkk <r3kinek0@gmail.com>
2022-11-01 20:00:54 +0100

    aaa

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+import time
+import socket
+
+localIP=socket.gethostbyname(socket.gethostname())
+localPORT=int(input("port: "))
+messagesize=1024
+
+ServerSock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+ServerSock.bind((localIP, localPORT))
+
+print("listening")
+
+while True:
+    bap = ServerSock.recvfrom(messagesize)
+    message=bap[0]
+    addr=bap[1]
+    print("message: ", message, " from: ", addr)
